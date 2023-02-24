c2ea12658c921c3c3d56efbc9ec2f6efd41c97d8
Edson Sousa <edson3711.es@gmail.com>
2023-02-23 22:09:09 -0300

    correção em user

--- src/main/java/com/fieb/tcc/academicologin/model/User.java
+++ src/main/java/com/fieb/tcc/academicologin/model/User.java
@@ -1,5 +1,21 @@
-package com.fieb.tcc.academicologin;
+package com.fieb.tcc.academicologin.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))	
 public class User {
-
-}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long d;
+	private String firstName;
+	private String lastName;
+	
+	private String email;
+	private String password;
+}	
