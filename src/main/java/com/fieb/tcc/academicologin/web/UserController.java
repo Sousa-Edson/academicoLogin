923691ec2e0db4bebda11e1adb177eae140af5b4
Sousa-Edson <edson3711.es@gmail.com>
2023-03-06 22:39:56 -0300

    atualização inserindo dto

---
+++ src/main/java/com/fieb/tcc/academicologin/web/UserController.java
@@ -0,0 +1,16 @@
+package com.fieb.tcc.academicologin.web;
+
+import org.springframework.stereotype.Controller;
+
+import com.fieb.tcc.academicologin.service.UserService;
+
+@Controller
+public class UserController {
+	
+//	private UserService userService;
+//
+//	public UserController (UserService userService) {
+//		
+//		this.userService = userService;
+//	}
+}
